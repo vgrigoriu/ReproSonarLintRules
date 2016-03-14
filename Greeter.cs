436b69e5efbfa40a15f720cd0f130cdf26660ad4
Victor Grigoriu <vgrigoriu@gmail.com>
2016-03-14 11:43:52 +0200

    Repro for S125 firing on documentation code

---
+++ Greeter.cs
@@ -0,0 +1,24 @@
+﻿namespace ReproSonarLintRules
+{
+    /// <summary>
+    /// Class that computes a greeting.
+    /// </summary>
+    /// <code>
+    ///     var greeter = new Greeter("World");
+    ///     var greeting = greeeter.GetGreeting();
+    /// </code>
+    public class Greeter
+    {
+        private readonly string whom;
+
+        public Greeter(string whom)
+        {
+            this.whom = whom;
+        }
+
+        public string GetGreeting()
+        {
+            return $"Hello, {whom}";
+        }
+    }
+}
